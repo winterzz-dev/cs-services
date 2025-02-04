def7620bf8df0a36ccd5b831808be9bbb7242a34
Max Zernyaev <winterzz@internet.ru>
2025-02-04 17:44:33 +0300

    feat(fast-stats-web): добавить компонент ссылок для fast-stats

--- apps/fast-stats-web/astro.config.mjs
+++ apps/fast-stats-web/astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '~illustration-maker-web': resolve(
+          __dirname,
+          '../illustration-maker-web/src'
+        ),
+      },
+    },
   },
 })
